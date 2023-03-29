7501cc90dd60902ceaa39a97760642d6e7d8472a
Sergey Landyrev <landyrev.serg@gmail.com>
2023-03-29 20:20:21 +0000

    iter1 test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	router := http.NewServeMux()
+	router.HandleFunc("/update", handleUpdate)
+
+	http.ListenAndServe(":8000", router)
+}
+
+func handleUpdate(w http.ResponseWriter, h *http.Request) {
+	fmt.Fprint(w, "Hello, world!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mrLandyrev/golang-metrics
+
+go 1.20
