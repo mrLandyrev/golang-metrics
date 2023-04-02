98bac6472a69fa9ddbc3c2d26bed20d2c50136f3
Sergey Landyrev <landyrev.serg@gmail.com>
2023-04-02 09:59:43 +0000

    iter1 add handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,15 +3,47 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
+)
+
+var (
+	couter = map[string]int64{}
+	guage  = map[string]float64{}
 )
 
 func main() {
 	router := http.NewServeMux()
-	router.HandleFunc("/update", handleUpdate)
+	router.HandleFunc("/update/", handleUpdate)
+	router.HandleFunc("/get/", handleGet)
 
-	http.ListenAndServe(":8000", router)
+	http.ListenAndServe(":8080", router)
 }
 
-func handleUpdate(w http.ResponseWriter, h *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+func handleUpdate(w http.ResponseWriter, r *http.Request) {
+	segments := strings.Split(r.URL.Path, "/")
+	metricType := segments[2]
+	metricName := segments[3]
+
+	switch metricType {
+	case "counter":
+		metricValue, _ := strconv.ParseInt(segments[4], 10, 64)
+		couter[metricName] += metricValue
+	case "guage":
+		metricValue, _ := strconv.ParseFloat(segments[4], 64)
+		guage[metricName] = metricValue
+	}
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	segments := strings.Split(r.URL.Path, "/")
+	metricType := segments[2]
+	metricName := segments[3]
+
+	switch metricType {
+	case "counter":
+		fmt.Fprint(w, couter[metricName])
+	case "guage":
+		fmt.Fprint(w, guage[metricName])
+	}
 }
